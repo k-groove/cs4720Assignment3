87bd98035e8a4156037ba0f419bc4c6748170546
Barry Oliver <i31770@gmail.com>
2017-02-21 19:00:42 -0500

    Sorting text entry works.

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-
-function addCell(){
-  var hidden = document.getElementsByClassName('hideText');
-  if(hidden.length){
-    var firstHidden = hidden[0];
-    firstHidden.classList.add('notFirst');
-    firstHidden.classList.remove('hideText');
-  }else{
-var container = document.getElementById('textContainer');
-var newLi = document.createElement('li');
-newLi.className += "notFirst";
-container.appendChild(newLi);
-var textEntry = document.createElement('input');
-newLi.appendChild(textEntry);
-}
-}
-
-function removeCell(){
-var list = document.getElementsByClassName('notFirst');
-if(list.length){
-var lastText = list[list.length-1];
-lastText.classList.add('hideText');
-lastText.classList.remove('notFirst');
-}
+function addCell() {
+    var hidden = document.getElementsByClassName('hideText');
+    if (hidden.length) {
+        var firstHidden = hidden[0];
+        firstHidden.classList.add('notFirst');
+        firstHidden.classList.remove('hideText');
+    } else {
+        var container = document.getElementById('textContainer');
+        var newLi = document.createElement('li');
+        newLi.className += "notFirst";
+        container.appendChild(newLi);
+        var textEntry = document.createElement('input');
+        textEntry.type = "text";
+        textEntry.value = "";
+        newLi.appendChild(textEntry);
+    }
 }
 
-function sortCells(){
-var items = document.getElementsByClassName('notFirst');
-console.log(items);
-var sorted = items.sort();
-items = sorted;
+function removeCell() {
+    var list = document.getElementsByClassName('notFirst');
+    if (list.length) {
+        var lastText = list[list.length - 1];
+        lastText.classList.add('hideText');
+        lastText.classList.remove('notFirst');
+    }
 }
 
-var inputText = document.getElementsByTagName('input');
-for(i=0;i<inputText.length;i++){
-  inputText[i].addEventListener('change', updateText(inputText[i]));
-}
-
-function updateText(element){
-  console.log("test");
+function sortCells() {
+  var temp = [];
+    var items = document.getElementsByTagName('input');
+    for(i=0;i<items.length;i++){
+      temp.push(items[i].value);
+    }
+    temp.sort();
+    for(i=0;i<items.length;i++){
+      items[i].value = temp[i];
+    }
 }
